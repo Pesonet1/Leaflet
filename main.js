c34fa53a19182ad0b8ba80536dd94f6e7862ecc5
Petteri Pesonen <teemu.p.pesonen@aalto.fi>
2016-01-30 13:06:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -264,11 +264,25 @@ function init() {
       radius = null
     }
   });
+  
+
+  
+  
+  var taso = L.geoJson(tasot, {
+    filter: function(feature, layer) {
+        return feature.properties.kayttotarkoitus == "Leikkipaikka";
+    },
+    onEachFeature: onEachFeature
+  }).addTo(map);
+  
+  
+  
 	
   karttatasot.addEventListener('change', function() {
     var checked = this.checked;
     if (checked) {
       tasot.addTo(map);
+      taso.addTo(map);
     } else {
       map.removeLayer(tasot);
       //tasot.clearLayers();
@@ -287,65 +301,6 @@ function init() {
       $box.prop("checked", false);
     }
   });
-
-
-
-
-  var filters = document.getElementById('filters');
-
-// Wait until the marker layer is loaded in order to build a list of possible
-// types. If you are doing this with another featureLayer, you should change
-// map.featureLayer to the variable you have assigned to your featureLayer.
-map.tasot.on('ready', function() {
-  // Collect the types of symbols in this layer. you can also just
-  // hardcode an array of types if you know what you want to filter on,
-  // like var types = ['foo', 'bar'];
-  var typesObj = {}, types = [];
-  var features = map.tasot.getGeoJSON().features;
-  for (var i = 0; i < features.length; i++) {
-    typesObj[features[i].properties['marker-symbol']] = true;
-  }
-  for (var k in typesObj) types.push(k);
-
-  var checkboxes = [];
-  // Create a filter interface.
-  for (var i = 0; i < types.length; i++) {
-    // Create an an input checkbox and label inside.
-    var item = filters.appendChild(document.createElement('div'));
-    var checkbox = item.appendChild(document.createElement('input'));
-    var label = item.appendChild(document.createElement('label'));
-    checkbox.type = 'checkbox';
-    checkbox.id = types[i];
-    checkbox.checked = true;
-    // create a label to the right of the checkbox with explanatory text
-    label.innerHTML = types[i];
-    label.setAttribute('for', types[i]);
-    // Whenever a person clicks on this checkbox, call the update().
-    checkbox.addEventListener('change', update);
-    checkboxes.push(checkbox);
-  }
-
-  // This function is called whenever someone clicks on a checkbox and changes
-  // the selection of markers to be displayed.
-  function update() {
-    var enabled = {};
-    // Run through each checkbox and record whether it is checked. If it is,
-    // add it to the object of types to display, otherwise do not.
-    for (var i = 0; i < checkboxes.length; i++) {
-      if (checkboxes[i].checked) enabled[checkboxes[i].id] = true;
-    }
-    map.tasot.setFilter(function(feature) {
-      // If this symbol is in the list, return true. if not, return false.
-      // The 'in' operator in javascript does exactly that: given a string
-      // or number, it says if that is in a object.
-      // 2 in { 2: true } // true
-      // 2 in { } // false
-      return (feature.properties['marker-symbol'] in enabled);
-    });
-  }
-});
-
-
 
 
 	
