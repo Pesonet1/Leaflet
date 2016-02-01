6f12629ff7473ccb5a7c2e5bbbfa4e190513aff3
Petteri Pesonen <teemu.p.pesonen@aalto.fi>
2016-02-01 19:16:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -348,7 +348,7 @@ function init() {
     var checked = this.checked;
     if (checked) {
       //Ei lisaa kartalle muita kuin Kesamaja-alueet... :/
-      filter = "Kesämaja-alue, Siirtolapuutarha, Viljelypalsta, Viljelypalsta-alue"
+      filter = "%Kesämaja-alue Siirtolapuutarha Viljelypalsta Viljelypalsta-alue"
       fillcolor = "#666699"
       update_layer();
     } else {
